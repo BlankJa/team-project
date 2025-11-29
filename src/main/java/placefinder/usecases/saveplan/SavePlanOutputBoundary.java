a6b6773d2c7b620992bdde5ed965a9843429d7ab
JohnJDH <johndh.jiang@mail.utoronto.ca>
2025-11-29 17:16:47 -0500

    Added Save Plan.

---
+++ src/main/java/placefinder/usecases/saveplan/SavePlanOutputBoundary.java
@@ -0,0 +1,8 @@
+package placefinder.usecases.saveplan;
+
+/**
+ * Output boundary for saving plan.
+ */
+public interface SavePlanOutputBoundary {
+    void present(SavePlanOutputData outputData);
+}
