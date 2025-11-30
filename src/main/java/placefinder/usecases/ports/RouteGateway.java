768a1332e8dedef05831092fd9464e79dd1ef0ed
MerpDurp <awesomesyt@gmail.com>
2025-11-30 18:02:49 -0500

    Added Route, leg and step entities and gateway for the API caller for compute route

---
+++ src/main/java/placefinder/usecases/ports/RouteGateway.java
@@ -0,0 +1,22 @@
+package placefinder.usecases.ports;
+
+import placefinder.entities.GeocodeResult;
+import placefinder.entities.PlanStop;
+import placefinder.entities.Route;
+
+import java.time.LocalTime;
+import java.util.List;
+
+public interface RouteGateway {
+
+    /**
+     * Compute the optimal walking route between plan stops.
+     *
+     * @param origin The location data of the start/end location
+     * @param startTime The starting time for the route
+     * @param stops List of the planned stops locations
+     * @return The optimal route as determined by the API
+     * @throws Exception if the API call fails
+     */
+    Route computeRoute(GeocodeResult origin, LocalTime startTime, List<PlanStop> stops) throws Exception;
+}
